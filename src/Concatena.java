4d22c08bc4de062f4d52c590339f5f9c25ee2580
tommaso.panciroli <tommypanci@gmail.com>
2025-02-07 21:45:22 +0100

    DONE EX.1

---
+++ src/Concatena.java
@@ -0,0 +1,8 @@
+public class Concatena {
+    public static void main(String[] args) {
+
+        int intero = 10;
+        String str ="Il valore è: " + (int) intero;
+        System.out.println(str);
+    }
+}
---
+++ src/Prodotto.java
@@ -0,0 +1,10 @@
+
+
+public class Prodotto {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        System.out.println("Il prodotto della moltiplicazione è: " + x * y);
+
+    }
+}
