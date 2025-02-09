f6027b39e4a736fb07aab0baec46a7f802a4e055
tommaso.panciroli <tommypanci@gmail.com>
2025-02-09 23:36:32 +0100

    EX.3 completed

--- src/day2/Esercizio3.java
+++ src/day2/Esercizio3.java
@@ -2,23 +2,25 @@ package day2;
 
 import java.util.Scanner;
 
-import java.util.Scanner;
 
 public class Esercizio3 {
     public static void main(String[] args) {
+        String stringa = "";
+        String finale = "";
         Scanner scanner = new Scanner(System.in);
-        String parola = "";
-        while (!parola.equals(":q")) {
-            System.out.println("Inserisci una parola (o :q per uscire): ");
-            parola = scanner.nextLine();
-            if (!parola.equals(":q")) {
-                String[] parole = parola.split(",");
-                for (int i = 0; i < parole.length; i++) {
-                    System.out.print(parole[i] + " ");
-                }
-                System.out.println();
+        while (!stringa.equals(":q")) {
+            System.out.println("Inserisci una stringa: ");
+            stringa = scanner.nextLine();
+            for (int i = 0; i < stringa.length(); i++) {
+                finale += stringa.charAt(i) + ",";
             }
+            System.out.println(finale);
+            finale = "";
         }
         scanner.close();
+
     }
+
+
 }
+
